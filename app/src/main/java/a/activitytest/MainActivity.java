35cb7b33bb3add5eef13e363144560b450dd3054
sojung kim <sojung kim>
2017-04-13 20:15:08 +0900

    #3 Event Programming

--- app/src/main/java/a/activitytest/MainActivity.java
+++ app/src/main/java/a/activitytest/MainActivity.java
@@ -1,5 +1,6 @@
 package a.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,12 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button butMain=(Button)findViewById(R.id.but_main);
-        butMain.setOnClickListener();
+        butMain.setOnClickListener(butMainHandler);
     }
     View.OnClickListener butMainHandler=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+            // getApplicationContext()도 가능
 
+            startActivity(intent);
         }
-    }
+    };
 }
