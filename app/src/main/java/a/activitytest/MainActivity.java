81ba9833b09021c740ec83af38d93a8ffc3d64e5
sojung kim <sojung kim>
2017-04-13 20:06:50 +0900

    #2 Layout

--- app/src/main/java/a/activitytest/MainActivity.java
+++ app/src/main/java/a/activitytest/MainActivity.java
@@ -2,6 +2,8 @@ package a.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button butMain=(Button)findViewById(R.id.but_main);
+        butMain.setOnClickListener();
+    }
+    View.OnClickListener butMainHandler=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 }
